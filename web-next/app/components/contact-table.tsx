e641e69cb511b158fb356934ab8ff4981ab3aabd
damanik27 <wilsondamanik27@gmail.com>
2026-01-16 11:18:20 +0700

    nembah th keterangan

--- web-next/app/components/contact-table.tsx
+++ web-next/app/components/contact-table.tsx
@@ -1,13 +1,13 @@
 const ContactTable = () => {
     return (
-        <table>
-            <thead>
+        <table className="w-full text-sm text-left text-gray-500">
+            <thead  className="text-sm text-gray-700 uppercase bg-amber-50">
                 <tr>
-                    <th>Name</th>   
-                    <th>Email</th>
-                    <th>Phone</th>
-                    <th>Actions</th>
-                    <th>Created at</th>                </tr>
+                    <th className="py-3 px-6">Name</th>   
+                    <th className="py-3 px-6">Email</th>
+                    <th className="py-3 px-6">Phone</th>
+                    <th className="py-3 px-6">Actions</th>
+                    <th className="py-3 px-6">Created at</th>                </tr>
             </thead>
         </table>
     );
