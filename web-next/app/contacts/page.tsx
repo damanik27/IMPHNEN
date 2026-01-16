1906f3b05e318c416bd6d481b6890ff16fadeb77
damanik27 <wilsondamanik27@gmail.com>
2026-01-16 11:07:11 +0700

    flex item

--- web-next/app/contacts/page.tsx
+++ web-next/app/contacts/page.tsx
@@ -1,10 +1,8 @@
 const Contact = () => {
     return (
-        <divcclassName="max-w-screen-md mx-auto p-5">
-        
-            
+        <div className="max-w-screen-md mx-auto p-5">
+        <div className="flex items-center justify-between gap-1 mb-5"></div>     
         </div>
     );
-
-}
+};
 export default Contact;
