ba15c28cbfbf8b955bf1c2b9f40528cc0d9a16a8
damanik27 <wilsondamanik27@gmail.com>
2026-01-16 16:27:06 +0700

    CreateButton

--- web-next/app/contacts/page.tsx
+++ web-next/app/contacts/page.tsx
@@ -1,11 +1,13 @@
 import ContactTable from "../components/contact-table";
 import Search from "../components/search";
+import { CreateButton } from "@/app/components/buttons"
 
 const Contact = () => {
   return (
     <div className="max-w-screen-md mx-auto p-5">
       <div className="flex items-center justify-between gap-1 mb-5">
         <Search />
+         <CreateButton />
       </div>
 
       <ContactTable />
