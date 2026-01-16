3b389eddae4245ec14964adad33d543cc56123f0
damanik27 <wilsondamanik27@gmail.com>
2026-01-16 11:29:59 +0700

    contractable

--- web-next/app/contacts/page.tsx
+++ web-next/app/contacts/page.tsx
@@ -1,7 +1,10 @@
+import ContactTable from "../components/contact-table";
+
 const Contact = () => {
     return (
         <div className="max-w-screen-md mx-auto p-5">
-        <div className="flex items-center justify-between gap-1 mb-5"></div>     
+        <div className="flex items-center justify-between gap-1 mb-5"></div>    
+        <ContactTable/> 
         </div>
     );
 };
