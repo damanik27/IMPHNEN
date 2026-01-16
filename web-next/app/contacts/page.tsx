a4a7308770f6b15e7ffd55feb7495358973d598d
damanik27 <wilsondamanik27@gmail.com>
2026-01-16 12:06:40 +0700

    buat seacrh

--- web-next/app/contacts/page.tsx
+++ web-next/app/contacts/page.tsx
@@ -1,9 +1,12 @@
 import ContactTable from "../components/contact-table";
+import Search from "../components/search";
 
 const Contact = () => {
     return (
         <div className="max-w-screen-md mx-auto p-5">
-        <div className="flex items-center justify-between gap-1 mb-5"></div>    
+        <div className="flex items-center justify-between gap-1 mb-5"> 
+        <search/>
+        </div>
         <ContactTable/> 
         </div>
     );
