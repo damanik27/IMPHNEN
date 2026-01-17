c0a2cf208626d18a72f009c4f71146d821231e69
raska26 <radityasandika51@gmail.com>
2026-01-17 20:31:36 +0700

    botton

--- web-next/app/components/create-form.tsx
+++ web-next/app/components/create-form.tsx
@@ -21,7 +21,9 @@ const CreateForm = () => {
           />
         </div>
         <bottom 
-          type="submit">
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-sm text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          px-5 py-3 text-center>
 
         </bottom>
           </label>
